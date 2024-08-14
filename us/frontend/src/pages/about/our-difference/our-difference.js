a56f59cc0415cde49908325649b4b53a2ccc88ed
D Luffy <akashwebroot@gmail.com>
2024-08-14 15:08:18 +0530

    more changes 14-08

--- us/frontend/src/pages/about/our-difference/our-difference.js
+++ us/frontend/src/pages/about/our-difference/our-difference.js
@@ -5,16 +5,16 @@ import Partners from "../../../components/partners-slider/partners";
 
 const testimonials = [
   {
-    imgSrc: "/images/Image 25.png",
-    partnerLogo: "/images/partnerLogos_SVG_CotswoldCo 1.png",
+    imgSrc: "/images/Image 25.svg",
+    partnerLogo: "/images/partnerLogos_SVG_CotswoldCo 1.svg",
     quote: "Flexible approach and strong relationship we have built together",
     description: "As an online retailer the final mile is perhaps the most crucial event in our customer journey and we have full confidence in BJS to always deliver the service our customers expect from us providing clear and regular communication to both us and our customers all the way through the process. As a business partner over many years we have particularly valued the flexible approach and strong relationship we have built together.",
     name: "Philippe Chainieux",
     title: "CEO"
   },
   {
-    imgSrc: "/images/Image 25 (1).png",
-    partnerLogo: "/images/partnerLogos_SVG_RicherSounds.png",
+    imgSrc: "/images/Image 25 (1).svg",
+    partnerLogo: "/images/partnerLogos_SVG_RicherSounds.svg",
     quote: "We are a demanding client and they keep us very happy",
     description: "BJS have given us consistently excellent service over a number of years. They handle our most important customers nationally… a vital role that must be carried out to the highest possible standard. We are a demanding client and they keep us very happy.",
     name: "Julian Richer",
@@ -24,28 +24,28 @@ const testimonials = [
 
 const teamTestimonials = [
   {
-    imgSrc: "/images/image 36.jpg",
+    imgSrc: "/images/image 36.svg",
     quote: "The service BJS offered was a perfect fit for us in terms of coverage and automation",
     description: "We used to have our own delivery fleet service and drivers, but the service BJS offered was a perfect fit for us in terms of coverage and automation. Outsourcing delivery to a specialist company was a logical decision and the best thing we’ve ever done. Choice for our customers, and tracking portals – has really helped our business to grow.",
     name: "Julian Bowen",
     title: "Operations Team",
-    logo: "/images/partnerLogos_SVG_JulianBowen.png"
+    logo: "/images/partnerLogos_SVG_JulianBowen.svg"
   },
   {
-    imgSrc: "/images/image 37.jpg",
+    imgSrc: "/images/image 37.svg",
     quote: "Exceptional & consistent service every time with helpful delivery options",
     description: "BJS provides Happy Beds with exceptional and consistent service every time with helpful delivery options. Happy Beds' customers are always complimenting the friendly and polite delivery drivers, so we couldn't be happier!",
     name: "Happy Beds",
     title: "Operations Team",
-    logo: "/images/image 39.png"
+    logo: "/images/image 39.svg"
   },
   {
-    imgSrc: "/images/image 38.jpg",
+    imgSrc: "/images/image 38.svg",
     quote: "The efficiency and professional nature of our deliveries does us proud",
     description: "BJS has been our go-to delivery partner for a couple of years. Why? Because we trust them like our own staff. Being a small but growing business, customer services needs to be at the heart of everything we do.",
     name: "Stewart Daniels",
     title: "Operations Director",
-    logo: "/images/image 40.png"
+    logo: "/images/image 40.svg"
   }
 ];
 
